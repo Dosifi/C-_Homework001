4cea42c8d206b8caf1f109533ab6edb19fede30d
Vladislav <vpfedotoff@gmail.com>
2022-12-18 19:35:18 +0300

    Task 003 update

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -1,9 +1,9 @@
 ﻿// Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
 Console.WriteLine("Введи число: ");
-int number1 = int.Parse(Console.ReadLine()!);
+int number = int.Parse(Console.ReadLine()!);
 
-int ostatok = number1 % 2;
+int ostatok = number % 2;
 
 if (ostatok == 0)
 Console.WriteLine("Число чётное");
